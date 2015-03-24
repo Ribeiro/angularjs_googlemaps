1dac270aa0dbe004287b5f38ccece858c63e67be
Ribeiro <geovanny.ribeiro@gmail.com>
2015-03-24 15:42:31 -0300

    added multiple markers on map

--- app.js
+++ app.js
@@ -1,23 +1,52 @@
 var app = angular.module('angular-maps', ['google-maps']);
 
 app.controller('MainCtrl', function($scope, $document) {
+    
   // map object
   $scope.map = {
     control: {},
     center: {
-        latitude: -3.791356871611455,
-        longitude:  -38.519200850000004
+      latitude: 37.78,
+        longitude: -122.41
     },
     zoom: 14
   };
-  
+
+ $scope.markers = [
+        {
+            id: 0,
+            coords: {
+                latitude: 37.7749295,
+                longitude: -122.4194155
+            },
+            data: 'restaurant'
+        },
+        {
+            id: 1,
+            coords: {
+                latitude: 37.79,
+                longitude: -122.42
+            },
+            data: 'house'
+        },
+        {
+            id: 2,
+            coords: {
+                latitude: 37.77,
+                longitude: -122.41
+            },
+            data: 'hotel'
+        }
+    ];
+
+
   // marker object
-  $scope.marker = {
-    center: {
-        latitude: -3.791356871611455,
-        longitude: -38.519200850000004
-    }
-  }
+  //$scope.marker = {
+    //center: {
+      //  latitude: -3.791356871611455,
+        //longitude: -38.519200850000004
+    //}
+  //}
   
   // instantiate google map objects for directions
   var directionsDisplay = new google.maps.DirectionsRenderer();
@@ -45,9 +74,14 @@ app.controller('MainCtrl', function($scope, $document) {
         directionsDisplay.setMap($scope.map.control.getGMap());
         directionsDisplay.setPanel(document.getElementById('directionsList'));
         $scope.directions.showList = true;
+
       } else {
          alert('Google route unsuccesfull!');
+
       }
+
     });
+
   }
+
 });
